f259a4f9309f5db25616a0ed7e0aae92d1043167
Victoria <mpelaez@cifpfbmoll.eu>
2021-11-25 02:16:49 +0100

    prueba trait

---
+++ capitulo5/trait.php
@@ -0,0 +1,54 @@
+<?php
+
+trait A
+{
+    private $a = 3;
+    protected $b;
+    public $c;
+}
+
+trait B
+{
+    use A;
+    private $d;
+    protected $e;
+    public $f;
+    protected $g = 7;
+}
+
+class C
+{
+    use B;
+    public function getPrivate()
+    {
+        return $this->a;
+    }
+    public function getProtecte(){
+        return $this->g;
+    }
+}
+
+$pruebaC = new C();
+echo "<pre>";
+var_dump($pruebaC);
+var_dump($pruebaC->getPrivate());
+var_dump($pruebaC->getProtecte());
+echo "</pre>";
+
+class D
+{
+    use A, B;
+    public function getPrivate()
+    {
+        return $this->a;
+    }
+    public function getProtecte(){
+        return $this->g;
+    }
+}
+$pruebaD = new D();
+echo "<pre>";
+var_dump($pruebaD);
+var_dump($pruebaD->getPrivate());
+var_dump($pruebaD->getProtecte());
+echo "</pre>";
