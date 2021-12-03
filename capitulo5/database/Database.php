7dd782db4d2141c9ea922d741fce2d46053a34f0
Victoria <mpelaez@cifpfbmoll.eu>
2021-12-03 02:01:30 +0100

    primera conexion basedatos

---
+++ capitulo5/database/Database.php
@@ -0,0 +1,28 @@
+<?php
+
+class Database
+{
+    private const HOST = "localhost";
+    private const DBNAME = "agenda";
+    private const USERNAME = "victoria";
+    private const PASSWORD = "victoria1988";
+    public static $conn;
+
+    private function __construct()
+    {
+
+    }
+
+    public static function getConnection()
+    {
+        try {
+            $conn = new PDO("mysql:host=" . Database::HOST . ";dbname=" . Database::DBNAME, Database::USERNAME, Database::PASSWORD);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Conexión realizada Satisfactoriamente";
+        } catch (PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+
+        return $conn;
+    }
+}
